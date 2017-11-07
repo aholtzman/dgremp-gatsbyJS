f6a00759eeae49074ee4295ba90281bda47cea69
Adam Holtzman <adam.holtzman@gmail.com>
2017-11-07 16:27:50 -0500

    add Family Run Gallery

---
+++ src/pages/Family-Run.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import TextWrap from '../components/TextWrap'
+import PhotoGallery from '../components/PhotoGallery'
+import data from '../images/7879_data'
+
+const FamilyRun = () => (
+  <div>
+    <TextWrap>
+      <h1>Chicago 78/79</h1>
+      <p>These photographs were made between 1978 and 1979 in various Chicago neighborhoods while working as an artist-in-residence with the Chicago Council on Fine Arts. Selections of this work have been exhibited at Alibi Fine Art (“Chicago 78/79”, June 2013 and the Harold Washington Library in Chicago (“Straight Into the Camera”, December 2015 – May 2016).</p>
+      <p>
+      “This was a dream assignment! All I was asked (and paid!) to do was to walk around selected neighborhoods and take pictures. Most people I approached were more than happy, and proud to pose for me.”
+      </p>
+      <br></br>
+      <hr></hr>
+    </TextWrap>
+    <PhotoGallery data={data} />
+  </div>
+)
+
+export default FamilyRun
